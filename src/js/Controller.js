5961f33006afa470259725afdb3e1db7f4380d70
ToyekaXV <juanjdv_200@hotmail.com>
2023-10-22 08:28:47 -0500

    Arreglo semantica y organizacion objeto

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -2,14 +2,13 @@ import Conexion from "./Conexion.js";
 import Game from "./Game.js";
 
 let numOfpairs = 9;
-let descubiertas = 0;
 
 let insConexion = new Conexion();
 let insGame = new Game();
 
 insGame.numCards = numOfpairs * 2;
 
-const consulta = async () => {
+const main = async () => {
   for (let i = 0; i < numOfpairs; i++) {
     let numRandom = Math.floor(Math.random() * 1000);
     let objPokemon = await insConexion.getData(`pokemon-form/${numRandom}`);
@@ -20,52 +19,17 @@ const consulta = async () => {
   crearalistener();
 };
 
-const clickCard = (element) => {
-  if (!element.classList.contains("card__girar")) {
-    descubiertas = descubiertas + 1;
-    element.classList.add("card__girar");
-    element.classList.add("active");
-    if (descubiertas === 2) {
-      setTimeout(() => {
-        let activeCards = document.querySelectorAll(".active");
-        if (
-          activeCards[1].lastChild.lastChild.textContent ==
-          activeCards[0].lastChild.lastChild.textContent
-        ) {
-          descubiertas = 0;
-        } else {
-          descubiertas = 0;
-          activeCards[1].classList.remove("card__girar");
-          activeCards[0].classList.remove("card__girar");
-        }
-        activeCards[1].classList.remove("active");
-        activeCards[0].classList.remove("active");
-
-        let allCards = document.querySelectorAll(".card");
-        for (let j = 0; j < allCards.length; j++) {
-          allCards[j].style.pointerEvents = "initial";
-        }
-      }, 1000);
-      console.log("Desactivar cliks");
-      let allCards = document.querySelectorAll(".card");
-      for (let j = 0; j < allCards.length; j++) {
-        allCards[j].style.pointerEvents = "none";
-      }
-    }
-  }
-};
-
 function crearalistener() {
   let cards = document.querySelectorAll(".card");
   for (var i = 0; i < cards.length; i++) {
     cards[i].addEventListener(
       "click",
       function () {
-        clickCard(this);
+        insGame.clickCard(this);
       },
       false
     );
   }
 }
 
-consulta();
+main();
--- src/js/Game.js
+++ src/js/Game.js
@@ -1,6 +1,7 @@
 class Game {
   numCards;
   arrayObjPkemon = [];
+  descubiertas = 0;
 
   createGame() {
     // barajar Arreglo
@@ -11,12 +12,46 @@ class Game {
     //Pintar arreglo
     for (let i = 0; i < this.numCards; i++) {
       let html = `<div class='cardBack'> <img class='cardBack__img' src='./assets/img/tcg-card-back.jpg' alt=''> </div> <div class='cardFront'> <img class='cardFront__img' src='${this.arrayObjPkemon[i].sprites.front_default}' alt=''><p class='cardFront__namePokemon'>${this.arrayObjPkemon[i].pokemon.name}</p></div>`;
-      var midiv = document.createElement("div");
-      midiv.setAttribute("class", "card");
-      midiv.innerHTML = html;
-      document.getElementById("panelGame").appendChild(midiv);
+      var myItem = document.createElement("li");
+      myItem.setAttribute("class", "card");
+      myItem.innerHTML = html;
+      document.getElementById("panelGame").appendChild(myItem);
     }
   }
+
+  clickCard = (element) => {
+    if (!element.classList.contains("card__girar")) {
+      this.descubiertas = this.descubiertas + 1;
+      element.classList.add("card__girar");
+      element.classList.add("active");
+      if (this.descubiertas === 2) {
+        setTimeout(() => {
+          let activeCards = document.querySelectorAll(".active");
+          if (
+            activeCards[1].lastChild.lastChild.textContent ==
+            activeCards[0].lastChild.lastChild.textContent
+          ) {
+            this.descubiertas = 0;
+          } else {
+            this.descubiertas = 0;
+            activeCards[1].classList.remove("card__girar");
+            activeCards[0].classList.remove("card__girar");
+          }
+          activeCards[1].classList.remove("active");
+          activeCards[0].classList.remove("active");
+
+          let allCards = document.querySelectorAll(".card");
+          for (let j = 0; j < allCards.length; j++) {
+            allCards[j].style.pointerEvents = "initial";
+          }
+        }, 1000);
+        let allCards = document.querySelectorAll(".card");
+        for (let j = 0; j < allCards.length; j++) {
+          allCards[j].style.pointerEvents = "none";
+        }
+      }
+    }
+  };
 }
 
 export default Game;
