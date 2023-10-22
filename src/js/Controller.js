12e075923880fac824c25ae14043009b00151677
ToyekaXV <juanjdv_200@hotmail.com>
2023-10-22 08:06:55 -0500

    Funcionalidad de girar cartas y arreglo de estilos

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -1,7 +1,8 @@
 import Conexion from "./Conexion.js";
 import Game from "./Game.js";
 
-let numOfpairs = 8;
+let numOfpairs = 9;
+let descubiertas = 0;
 
 let insConexion = new Conexion();
 let insGame = new Game();
@@ -15,7 +16,56 @@ const consulta = async () => {
     insGame.arrayObjPkemon.push(objPokemon);
     insGame.arrayObjPkemon.push(objPokemon);
   }
-  await insGame.createGame();
+  insGame.createGame();
+  crearalistener();
 };
+
+const clickCard = (element) => {
+  if (!element.classList.contains("card__girar")) {
+    descubiertas = descubiertas + 1;
+    element.classList.add("card__girar");
+    element.classList.add("active");
+    if (descubiertas === 2) {
+      setTimeout(() => {
+        let activeCards = document.querySelectorAll(".active");
+        if (
+          activeCards[1].lastChild.lastChild.textContent ==
+          activeCards[0].lastChild.lastChild.textContent
+        ) {
+          descubiertas = 0;
+        } else {
+          descubiertas = 0;
+          activeCards[1].classList.remove("card__girar");
+          activeCards[0].classList.remove("card__girar");
+        }
+        activeCards[1].classList.remove("active");
+        activeCards[0].classList.remove("active");
+
+        let allCards = document.querySelectorAll(".card");
+        for (let j = 0; j < allCards.length; j++) {
+          allCards[j].style.pointerEvents = "initial";
+        }
+      }, 1000);
+      console.log("Desactivar cliks");
+      let allCards = document.querySelectorAll(".card");
+      for (let j = 0; j < allCards.length; j++) {
+        allCards[j].style.pointerEvents = "none";
+      }
+    }
+  }
+};
+
+function crearalistener() {
+  let cards = document.querySelectorAll(".card");
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener(
+      "click",
+      function () {
+        clickCard(this);
+      },
+      false
+    );
+  }
+}
 
 consulta();
--- src/js/Game.js
+++ src/js/Game.js
@@ -10,7 +10,7 @@ class Game {
 
     //Pintar arreglo
     for (let i = 0; i < this.numCards; i++) {
-      let html = `<div class='cardBack none'> <img class='cardBack__img' src=''./assets/img/tcg-card-back.jpg' alt=''> </div> <div class='cardFront'> <img class='cardFront__img' src='${this.arrayObjPkemon[i].sprites.front_default}' alt=''><p class='cardFront__namePokemon'>${this.arrayObjPkemon[i].pokemon.name}</p></div>`;
+      let html = `<div class='cardBack'> <img class='cardBack__img' src='./assets/img/tcg-card-back.jpg' alt=''> </div> <div class='cardFront'> <img class='cardFront__img' src='${this.arrayObjPkemon[i].sprites.front_default}' alt=''><p class='cardFront__namePokemon'>${this.arrayObjPkemon[i].pokemon.name}</p></div>`;
       var midiv = document.createElement("div");
       midiv.setAttribute("class", "card");
       midiv.innerHTML = html;
